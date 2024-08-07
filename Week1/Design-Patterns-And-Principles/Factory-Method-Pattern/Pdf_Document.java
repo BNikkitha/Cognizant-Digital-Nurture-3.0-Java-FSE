9d95db28cf39026904d10396cb81fde16c8332b2
BNikkitha <159457459+BNikkitha@users.noreply.github.com>
2024-08-07 18:57:01 +0530

    Create Pdf_Document.java

---
+++ Week1/Design-Patterns-And-Principles/Factory-Method-Pattern/Pdf_Document.java
@@ -0,0 +1,11 @@
+public class Pdf_Document implements Document {
+    @Override
+    public void open() {
+        System.out.println("Opening PDF document.");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("Closing PDF document.");
+    }
+}
