53a7514cf46812e245a673a120e0c4e72570e201
BNikkitha <159457459+BNikkitha@users.noreply.github.com>
2024-08-07 12:40:23 +0530

    Create LoggerTest.java

---
+++ Week1/Design-Patterns-And-Principles/Singleton/LoggerTest.java
@@ -0,0 +1,18 @@
+public class LoggerTest {
+    public static void main(String[] args) {
+        // Get two instances of Logger
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        // Test that both references point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 refer to the same instance.");
+        } else {
+            System.out.println("Different instances were created.");
+        }
+
+        // Log a message to verify functionality
+        logger1.log("This is a test log message.");
+        logger2.log("This message should be logged by the same instance.");
+    }
+}
