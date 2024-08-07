723a56dd9352bb752d9baffe4278bdca1213eb38
BNikkitha <159457459+BNikkitha@users.noreply.github.com>
2024-08-08 00:55:31 +0530

    Update Pdf_Document_Factory.java

--- Week1/Design-Patterns-And-Principles/Factory-Method-Pattern/Pdf_Document_Factory.java
+++ Week1/Design-Patterns-And-Principles/Factory-Method-Pattern/Pdf_Document_Factory.java
@@ -1,6 +1,6 @@
 public class Pdf_Document_Factory extends DocumentFactory {
     @Override
     public Document createDocument() {
-        return new PdfDocument();
+        return new Pdf_Document();
     }
 }
