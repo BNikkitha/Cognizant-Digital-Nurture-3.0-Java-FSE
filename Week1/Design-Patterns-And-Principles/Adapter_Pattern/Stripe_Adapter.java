827d322022aed0f0dca84958daa0180ac5e7899e
BNikkitha <159457459+BNikkitha@users.noreply.github.com>
2024-08-09 13:55:23 +0530

    Create Stripe_Adapter.java

---
+++ Week1/Design-Patterns-And-Principles/Adapter_Pattern/Stripe_Adapter.java
@@ -0,0 +1,12 @@
+public class Stripe_Adapter implements PaymentProcessor {
+    private StripeGateway stripeGateway;
+
+    public StripeAdapter(StripeGateway stripeGateway) {
+        this.stripeGateway = stripeGateway;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        stripeGateway.charge(amount);
+    }
+}
