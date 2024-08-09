e52db3fe468eee749b4daf27a4d44c48ed6cc3bb
BNikkitha <159457459+BNikkitha@users.noreply.github.com>
2024-08-09 13:54:50 +0530

    Create PayPal_Adapter.java

---
+++ Week1/Design-Patterns-And-Principles/Adapter_Pattern/PayPal_Adapter.java
@@ -0,0 +1,12 @@
+public class PayPal_Adapter implements PaymentProcessor {
+    private PayPalGateway payPalGateway;
+
+    public PayPalAdapter(PayPalGateway payPalGateway) {
+        this.payPalGateway = payPalGateway;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        payPalGateway.makePayment(amount);
+    }
+}
